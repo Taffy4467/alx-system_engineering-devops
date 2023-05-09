3815020c3c3f147b2de2c7259d45f1a92745ef66
Tafadzwa <gandarit4467@gmail.com>
2023-05-09 08:23:09 -0700

    recurse trial

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
+"""The Reddit API uses pagination for separating pages of responses"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+def recurse(subreddit, hot_list=[], after=""):
+    """Querying Reddit API, and returns all
+    hot articles for a given subreddit."""
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'API Project by davixcky'}
+    arg1 = {"limit": 100, "after": after}
+    resp = requests.get(url, params=arg1, headers=headers)
+    list_a = resp.json().get('data', {}).get('children', None)
+    pagination = resp.json().get('data', {}).get('after', None)
+
+    if pagination is not None:
+
+        if list_a:
+            for item in list_a:
+                hot_list.append(item.get("data").get("title"))
+
+        if pagination is not None:
+            recurse(subreddit, hot_list, pagination)
+
+        return hot_list
+
+    else:
         return None
-
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
