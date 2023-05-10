9acfa31b326175b3d1fd31d8a8873050e84aec8d
Tafadzwa <gandarit4467@gmail.com>
2023-05-10 06:56:46 -0700

    redoing task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,24 +1,6 @@
-# 0-strace_is_your_friend.pp
+# Fixes bad `phpp` extensions to `php`
 
-# Ensure Apache package is installed
-package { 'apache2':
-  ensure => installed,
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
-# Replace the faulty Apache configuration file with the correct one
-file { '/etc/apache2/apache2.conf':
-  ensure => file,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
-  source => 'puppet:///modules/my_module/apache2.conf',  # Replace with the correct source path
-  notify => Service['apache2'],
-}
-
-# Ensure Apache service is running and enabled
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => Package['apache2'],
-}
-
